ffbb6c8e6d204a350f417f6bf2459db7526cad75
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-02-20 22:27:35 +1100

    Complete contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,30 @@
 export function contactTab() {
     const container = document.createElement("div");
+    container.classList.add("container");
+
+    const contactsHeading = document.createElement("div");
+    contactsHeading.classList.add("heading-container");
+    const headingTextCard = document.createElement("div");
+    headingTextCard.classList.add("heading-text-card");
+    const heading = document.createElement("h1");
+    heading.textContent = "Contact";
+
+    headingTextCard.append(heading);
+    contactsHeading.append(headingTextCard);
+
+    const card = document.createElement("div");
+    const detailsCard = document.createElement("div");
+    const phoneHeading = document.createElement("h3");
+    phoneHeading.textContent = "Phone Number";
+    const number = document.createElement("p");
+    number.textContent = "+61 23 456 789";
+    const emailHeading = document.createElement("h3");
+    emailHeading.textContent = "Email";
+    const email = document.createElement("p");
+    email.textContent = "firehorserestaurant@totallyreal.com";
+
+    detailsCard.append(phoneHeading, number, emailHeading, email);
+    card.append(detailsCard);
+    container.append(contactsHeading, card);
     return container;
 }
